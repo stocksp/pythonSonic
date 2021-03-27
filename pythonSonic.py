aca0036bd86c8cba86a35ec62069e513f5388958
Paul Stocks <paul.stocks@gmail.com>
2021-03-27 09:40:43 -0700

    more

--- pythonSonic.py
+++ pythonSonic.py
@@ -5,7 +5,7 @@ import time
 #import board
 #import adafruit_hcsr04
 from datetime import datetime, timezone, timedelta
-
+from statistics import mean
 import asyncio
 from pymongo import MongoClient
 
@@ -25,37 +25,59 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    try:
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
 
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-    #convert to inches
-    return distance * 0.3937008
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+        #convert to inches
+        return distance * 0.3937008
+    except Exception as err:
+        print(f'Error reading sensor {err}', flush=True)
+        return 0
 
 async def sonicSensor():
+    distList = []
+    lastUpdateValue = 0.0
     try:
         while True:
-            dist = distance()
-            print("Measured Distance = %.1f cm" % dist)
+            theDist = distance()
+            print("Measured Distance = %.1f cm" % theDist)
+            if len(distList) > 0:
+                previousAve = mean(distList)
+            else:
+                previousAve = 0.0
+            if theDist != 0:
+                distList.append(theDist)
+            # keep list at 6 AND remove errors of 0
+            if len(distList) > 6:
+                distList.pop(0)
+            currentAve = mean(distList)
+            msg = (
+                f'{theDist:.1f} inches previous ave {previousAve:.1f}'
+                f'current ave{currentAve:.1f}'
+                f'lastUpdateValue {lastUpdateValue}'
+            )
+            print(msg)
             await asyncio.sleep(5)
 
         # Reset by pressing CTRL + C
