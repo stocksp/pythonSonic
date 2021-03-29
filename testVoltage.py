0396aac14f7d7ab77a751a071b6df41a6f8ee059
Paul Stocks <paul.stocks@gmail.com>
2021-03-28 20:22:39 -0700

    revert

--- testVoltage.py
+++ testVoltage.py
@@ -1,5 +1,4 @@
 import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
 import busio
 # import digitalio
 import board
@@ -14,6 +13,21 @@ mcp = MCP.MCP3008(spi, 0)
 
 # create an analog input channel on pin 5
 chan0 = AnalogIn(mcp, MCP.P0)
+counter = 0
+sampleSize = 50
+rawData = []
 
-print('Raw ADC Value: ', chan0.value)
-print('ADC Voltage: ' + str(chan0.voltage * 5) + 'V')
+while counter < sampleSize:
+    # if counter == 50:
+    #     print('Raw ADC Value 6: ', chan5.value)
+    #     print('ADC Voltage 6: ' + str(chan5.voltage) + 'V')
+    #     print('Raw ADC Value 4: ', chan3.value)
+    #     print('ADC Voltage 4: ' + str(chan3.voltage) + 'V')
+
+    rawData.append(
+        (chan0.value, chan0.voltage)
+    )  # noqa
+    chan0 = AnalogIn(mcp, MCP.P0)
+   
+    counter += 1
+print(rawData)
