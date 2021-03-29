c5d1c9c5effb84ecf4254f3fc35f14ba06b19dd0
Paul Stocks <paul.stocks@gmail.com>
2021-03-29 13:20:59 -0700

    z

--- testVoltage.py
+++ testVoltage.py
@@ -3,7 +3,7 @@ from MCP3008 import MCP3008
 adc = MCP3008()
 value = adc.read( channel = 0 ) 
 voltage = (value * 5 * 0.815/ 1023.0 * 3.3)
-voltage2 = (value * 5 * 0.815) / (1023.0 * 3.3)
+voltage2 = (value * 5 * 0.815) / 1023.0 * 3.3
 print("Applied voltage: %.2f" % (value * 5 * 0.815/ 1023.0 * 3.3) )
 print(f'Voltage: {voltage:.2f}')
 print(f'Voltage2: {voltage2}')
