f4ab9a916b1239d2ed215ad50ada920ac468b567
Paul Stocks <paul.stocks@gmail.com>
2021-03-29 11:54:50 -0700

    using spidev for voltage

--- testVoltage.py
+++ testVoltage.py
@@ -1,19 +1,26 @@
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
-import busio
-# import digitalio
-import board
+import spidev
 
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-# create the cs (chip select)
-# cs = digitalio.DigitalInOut(board.D22)
+class MCP3008:
+    def __init__(self, bus=0, device=0):
+        self.bus, self.device = bus, device
+        self.spi = spidev.SpiDev()
+        self.open()
+        self.spi.max_speed_hz = 1000000  # 1MHz
 
-# create the mcp object
-mcp = MCP.MCP3008(spi, 0)
+    def open(self):
+        self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000  # 1MHz
 
-# create an analog input channel on pin 5
-chan0 = AnalogIn(mcp, MCP.P0)
+    def read(self, channel=0):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
+        return data
 
-print('Raw ADC Value: ', chan0.value)
-print('ADC Voltage: ' + str(chan0.voltage * 5) + 'V')
+    def close(self):
+        self.spi.close()
+
+
+adc = MCP3008()
+value = adc.read(channel=0)  # You can of course adapt the channel to be read out
+print("Applied voltage: %.2f" % (value * 5 / 1023.0 * 3.3))
